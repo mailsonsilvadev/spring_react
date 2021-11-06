c267f696c18b6302ed8167acc36ee818f5325d7d
Mailson Silva <mls.sh@hotmail.com>
2021-11-06 17:20:05 -0300

    Componentes básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Óia aí o guri</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Óia aí o guri</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
